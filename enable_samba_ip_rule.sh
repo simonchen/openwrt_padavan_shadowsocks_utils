93c8a3333dc773a17414f67a0a9aa3a1a359b5c0
xychenxy <chenxinyi1978@hotmail.com>
2022-12-01 13:32:25 +0800

    Update and rename ip_rule_delete.sh to enable_samba_ip_rule.sh

--- enable_samba_ip_rule.sh
+++ enable_samba_ip_rule.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # delete any ip rule that have comment with 'netbios'
 
+# deleting existing rules
 while ip_rule_num=$(iptables -L INPUT --line-numbers | grep netbios | cut -d" " -f1)
 do
     if [ -z $ip_rule_num ]; then
@@ -12,3 +13,8 @@ do
         break
     done
 done
+
+# adding new rules
+iptables -I INPUT 1 -p udp -m multiport --dport 137,138 -j ACCEPT 
+iptables -I INPUT 1 -p tcp -m state --state NEW,RELATED,ESTABLISHED -m multiport --dport 139,445 -j ACCEPT
+iptables -I INPUT -p icmp -j ACCEPT
