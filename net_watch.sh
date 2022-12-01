b632d1797f098c18491050aebd544618219b4bcc
xychenxy <chenxinyi1978@hotmail.com>
2022-12-01 21:06:53 +0800

    Update net_watch.sh

--- net_watch.sh
+++ net_watch.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 # detecting if the child router is disconnected from parent router with http://192.168.0.1
+#!/bin/sh
+# detecting if the child router is disconnected from parent router
 
 LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
-wget --spider --quiet http://192.168.0.1
-if [ "$?" == "0" ]; then
+ok=0
+for i in {1..10};do
+  wget --spider --quiet http://192.168.0.1
+  if [ "$?" == "0" ]; then
+    ok=1
+    break
+  fi
+  sleep 1
+done
+echo network is $ok
+
+if [ $ok == 1 ]; then
         echo '['$LOGTIME'] No Problem.'
+        echo 'check print service port 9100'
         net_port=$(netstat -a | grep "9100")
         echo "$net_port"
         if [ -z "$net_port" ]; then
                 echo "print service was down. restarting..."
-                /etc/init.d/p910nd start > /dev/null
+                /etc/init.d/p910nd restart > /dev/null
         fi
         exit 0
 else
         echo '['$LOGTIME'] Problem decteted, restarting network.'
-        # restarts USB printer
-        /etc/init.d/p910nd restart > /dev/null
-        # restarts network
         /etc/init.d/network restart >/dev/null
 fi
