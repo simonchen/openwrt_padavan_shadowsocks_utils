082fe446b8e592b05f586630bfaf8042897fa7b4
xychenxy <chenxinyi1978@hotmail.com>
2022-12-24 15:15:24 +0800

    Create ss_proxy.sh

---
+++ ss_proxy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# 支持并发4个上游dnsproxy服务
+pre_start() {
+    echo "ss-tproxy 启动前执行脚本"
+    logger -t "【删除dnsmasq定制配置】" "完成"
+    sed -i '/all-servers/d' /etc/storage/dnsmasq/dnsmasq.conf
+    sed -i '/server=127.0.0.1#8054/d' /etc/storage/dnsmasq/dnsmasq.conf
+    sed -i '/server=127.0.0.1#8055/d' /etc/storage/dnsmasq/dnsmasq.conf
+    sed -i '/server=127.0.0.1#8056/d' /etc/storage/dnsmasq/dnsmasq.conf
+    logger -t "【添加dnsmasq定制配置】" "完成"
+    echo all-servers >> /etc/storage/dnsmasq/dnsmasq.conf
+    echo server=127.0.0.1#8054 >> /etc/storage/dnsmasq/dnsmasq.conf
+    echo server=127.0.0.1#8055 >> /etc/storage/dnsmasq/dnsmasq.conf
+    echo server=127.0.0.1#8056 >> /etc/storage/dnsmasq/dnsmasq.conf
+}
+post_start() {
+    echo "ss-tproxy 启动后执行脚本"
+    logger -t "【启动多个dnsproxy - 5053,5054,5055,5056】" "完成"
+    killall dnsproxy && dnsproxy -d -p 8053 && dnsproxy -d -p 8054 && dnsproxy -d -p 8055 && dnsproxy -d -p 8056
+}
+pre_stop() {
+    echo "ss-tproxy 停止前执行脚本"
+    
+}
+post_stop() {
+    echo "ss-tproxy 停止后执行脚本"
+    
+}
