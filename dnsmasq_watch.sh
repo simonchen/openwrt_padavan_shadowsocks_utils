24d0351e3d5e7cca0513c9bb8b728afb34f8b2f1
xychenxy <chenxinyi1978@hotmail.com>
2022-12-27 21:11:30 +0800

    dnsmasq超过5个进程后自动重启
        logger -t "【监控dnsmasq】" "完成"
        /etc/storage/apps/dnsmasq_watch.sh stop
        /etc/storage/apps/dnsmasq_watch.sh >/dev/null 2>&1 </dev/null &

--- dnsmasq_watch.sh
+++ dnsmasq_watch.sh
@@ -8,7 +8,7 @@ fi
 while true; do
   instances=$(ps | grep /usr/sbin/dnsmasq | wc -l)
   echo $instances
-  [ "$instances" -le 2 ] && sleep 2 && continue
-  logger -s -t "dnsmasq instances > 2" "restart!"
-  #killall dnsmasq && /usr/sbin/dnsmasq
+  [ "$instances" -le 5 ] && sleep 2 && continue
+  logger -s -t "dnsmasq instances > 5" "restart!"
+  killall dnsmasq && /usr/sbin/dnsmasq
 done
