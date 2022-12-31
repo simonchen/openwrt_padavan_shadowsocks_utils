28391e7b0cf8355c18b117e9d6f30ea0682aafc4
xychenxy <chenxinyi1978@hotmail.com>
2023-01-01 00:39:28 +0800

    dnsmasq并发4个上游dnsproxy服务 / 监控dnsmasq

--- ss_proxy.sh
+++ ss_proxy.sh
@@ -12,10 +12,14 @@ pre_start() {
     echo server=127.0.0.1#8054 >> /etc/storage/dnsmasq/dnsmasq.conf
     echo server=127.0.0.1#8055 >> /etc/storage/dnsmasq/dnsmasq.conf
     echo server=127.0.0.1#8056 >> /etc/storage/dnsmasq/dnsmasq.conf
+    logger -t "【监控dnsmasq】" "完成"
+    /etc/storage/apps/dnsmasq_watch.sh stop
+    /etc/storage/apps/dnsmasq_watch.sh >/dev/null 2>&1 </dev/null &
 }
 post_start() {
     echo "ss-tproxy 启动后执行脚本"
     logger -t "【启动多个dnsproxy - 5053,5054,5055,5056】" "完成"
+    sed -i '/dns-forward-max=1000/d' /etc/storage/dnsmasq/dnsmasq.conf
     killall dnsmasq && /usr/sbin/dnsmasq
     killall dnsproxy && dnsproxy -d -p 8053 && dnsproxy -d -p 8054 && dnsproxy -d -p 8055 && dnsproxy -d -p 8056
 }
