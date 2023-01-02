cf47d1b507678df3b7f12a113c1c2abe25938454
xychenxy <chenxinyi1978@hotmail.com>
2023-01-02 20:51:53 +0800

    dnsmasq超过10个进程且可用内存太低，自动重启

--- dnsmasq_watch.sh
+++ dnsmasq_watch.sh
@@ -7,8 +7,14 @@ fi
 
 while true; do
   instances=$(ps | grep /usr/sbin/dnsmasq | wc -l)
-  echo $instances
-  [ "$instances" -le 5 ] && sleep 1 && continue
-  logger -s -t "dnsmasq instances > 5" "restart!"
+  min_free_kb=$(expr $(cat /proc/sys/vm/min_free_kbytes) \* 3)
+  cur_free_kb=$(cat /proc/meminfo | grep 'MemFree:' | sed -E 's/MemFree\:[^0-9]+(.+) kb/\1/i')
+  f=$(expr $cur_free_kb \<= $min_free_kb)
+  echo 'dnsmasq insts='$instances
+  echo 'min_free_kb='$min_free_kb
+  echo 'cur_free_kb='$cur_free_kb
+  echo 'f='$f
+  [ $f -eq 0 ] && [ "$instances" -le 10 ] && sleep 1 && continue
+  logger -s -t ">>> dnsmasq instances > 10 and the available memory is too low: $cur_free_kb kB" "restart!"
   killall dnsmasq && /usr/sbin/dnsmasq
 done
