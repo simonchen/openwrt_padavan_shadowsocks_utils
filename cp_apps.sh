62e62d869f2e1adbc3da492a4e1d9b908a812d40
xychenxy <chenxinyi1978@hotmail.com>
2023-02-08 12:35:31 +0800

    Padavan-父子双进程自动守护(科学上网,kcptun,udp2raw)

---
+++ cp_apps.sh
@@ -0,0 +1,329 @@
+#!/bin/sh
+#copyright by simonchen
+# the script run as daemon prieodically binding the symbolic link in /opt/bin for the existing apps (kcptun/udp2raw)
+# auto-monitor and restart kcptun/udp2raw with specific parameters - [server] [ports] [key]
+# udp2raw listen [::1]:8388
+# kcptun listen [::1]:3333
+# In addition,
+# 1. disabling the logs with crond, ntp sync.
+# 2. auto-restart kcptun/udp2raw if it continously failed to visit google.com
+# 3. Replace dnsmasq with latest version 2.89
+# 4. Reboot router if internet connection fails on continuous 10 retries.
+
+basedir=$(cd $(dirname $0) && pwd)
+basename=$(basename $0)
+
+server="$1"
+ports="$2"
+key="$3"
+if [[ -z "$server" || -z "$ports" || -z "$key" ]]; then
+  echo "Usage: $basename [server] [ports] [key]"
+  exit 1
+fi
+
+myname="padavan"
+daemon_sh="$myname-d.sh"
+daemon_status="$myname-d.status"
+daemon_sub_sh="$myname-ds.sh"
+daemon_sub_status="$myname-ds.status"
+
+# Reset daemon 
+pgrep $myname | xargs kill >/dev/nulll 2>&1 
+rm -f /tmp/$daemon_sub_status 
+rm -f /tmp/$daemon_status
+
+# Replace dnsmasq with latest version 2.89
+restart_dnsmasq() {
+  if [ -f $basedir/dnsmasq ]; then
+    umount -fl /usr/sbin/dnsmasq
+    mount -o bind $basedir/dnsmasq /usr/sbin/dnsmasq
+    killall dnsmasq >/dev/null 2>&1
+    /usr/sbin/dnsmasq 2>&1 &
+  fi
+}
+
+##################################################################
+#
+#  sub-daemon守护
+#
+##################################################################
+cat >/tmp/$daemon_sub_sh <<'EOF'
+#!/bin/sh
+#daemon sub-process
+EOF
+cat <<EOF >> /tmp/$daemon_sub_sh
+logger -s -t "【 sub-daemon 本地应用守护】" "启动"
+basedir="$basedir"
+daemon_sh="$daemon_sh"
+daemon_status="$daemon_status"
+daemon_sub_sh="daemon_sub_sh"
+daemon_sub_status="$daemon_sub_status"
+
+server="$server"
+ports="$ports"
+key="$key"
+EOF
+cat >>/tmp/$daemon_sub_sh <<'EOF'
+udp2raw_port_file=/tmp/udp2raw_port.txt
+
+read_total_secs() {
+  s=""
+  if [ -f "/tmp/$daemon_sub_status" ]; then
+    s="$(cat /tmp/$daemon_sub_status | grep "runtime=" | sed -E "s/runtime=([0-9]+)/\1/")"
+  fi
+  echo "$s"
+}
+
+write_total_secs() {
+  s="$1"
+  if [ -f /tmp/$daemon_sub_status ]; then
+    sed -i "/runtime=/d" /tmp/$daemon_sub_status
+  fi
+  echo "runtime=$s" >> /tmp/$daemon_sub_status
+}
+
+get_udp2raw_port() {
+  if [ ! -f "$udp2raw_port_file" ]; then
+    echo ""
+    return
+  fi
+  echo $(cat "$udp2raw_port_file")
+}
+
+set_udp2raw_port() {
+  echo $1 > $udp2raw_port_file
+}
+
+clear_udp2raw_port() {
+  echo "" > $udp2raw_port_file
+}
+
+selfkill_secs=3600 # must be even number
+interval_secs=21600 # must be even number
+total_secs=$(read_total_secs)
+if [ -z "$total_secs" ]; then
+  total_secs=0
+fi
+sleep_secs=2
+while true; do
+  avail_port=  
+  for port in $ports; do
+    cur_port=$(get_udp2raw_port)
+    #echo "cur_port="$cur_port
+    val=$(echo "$cur_port" | grep $port)
+    #echo "val="$val
+    if [ ! -z "$val" ]; then
+      continue
+    fi
+    if [ -z "$cur_port" ]; then
+      cur_port="$port"
+    else
+      cur_port="$cur_port:$port"
+    fi
+    set_udp2raw_port "$cur_port"
+    avail_port=$port
+    break
+  done
+
+  if [ -z "$avail_port" ]; then
+    clear_udp2raw_port
+    continue
+  fi
+  #echo "avail_port="$avail_port
+  if [[ -z "$(pgrep udp2raw)" && -f "/opt/bin/udp2raw" ]]; then
+    logger -t "【启动udp2raw】" "用服务端口$avail_port"
+    killall udp2raw >/dev/null 2>&1 
+    /opt/bin/udp2raw --fix-gro -c -l[::1]:3333 -r$server:$avail_port -a -k "$key" --cipher-mode xor --raw-mode icmp >/dev/null 2>&1 &
+  fi
+  while true; do
+    # see if main daemon_sh is dead?
+    if [ -z "$(pgrep $daemon_sh)" ]; then
+      killall $daemon_sh >/dev/null 2>&1
+      chmod +x /tmp/$daemon_sh && /tmp/$daemon_sh 2>&1 &
+    fi
+    sleep $sleep_secs
+    total_secs=$(expr $total_secs \+ $sleep_secs)
+    $(write_total_secs $total_secs)
+    total_mins=$(expr $total_secs \/ 60)
+    if [ $(expr $total_secs \% $interval_secs) -eq 0 ]; then
+      logger -t "【udp2raw】" "已经运行$total_mins分钟, 开始切换端口."
+      break
+    fi
+    if [[ $(expr $total_secs \% $selfkill_secs) -eq 0 && ! -z "$(pgrep $daemon_sh)" ]]; then
+      logger -t "【sub-daemon守护】" "已经运行$total_mins分钟, 自重启."
+      killall $daemon_sub_sh >/dev/null 2>&1
+    fi
+  done
+done
+EOF
+
+##################################################################
+#
+#  main-daemon 本地应用守护
+#
+##################################################################
+logger -s -t "【 main-daemon 本地应用守护】" "启动"
+
+cat >/tmp/$daemon_sh <<'EOF'
+#!/bin/sh
+#auto-link local apps to /opt/bin/
+#auto-monitor apps such as kcptun/udp2raw 
+EOF
+cat <<EOF >> /tmp/$daemon_sh
+key="$key"
+basedir="$basedir"
+daemon_sh="$daemon_sh"
+daemon_status="$daemon_status"
+daemon_sub_sh="$daemon_sub_sh"
+daemon_sub_status="$daemon_sub_status"
+EOF
+cat >>/tmp/$daemon_sh <<'EOF'
+
+read_total_secs() {
+  s=""
+  if [ -f "/tmp/$daemon_status" ]; then
+    s="$(cat /tmp/$daemon_status | grep "runtime=" | sed -E "s/runtime=([0-9]+)/\1/")"
+  fi
+  echo "$s"
+}
+
+write_total_secs() {
+  s="$1"
+  if [ -f /tmp/$daemon_status ]; then
+    sed -i "/runtime=/d" /tmp/$daemon_status
+  fi
+  echo "runtime=$s" >> /tmp/$daemon_status
+}
+
+# Replace dnsmasq with latest version 2.89
+restart_dnsmasq() {
+  if [ -f $basedir/dnsmasq ]; then
+    umount -fl /usr/sbin/dnsmasq
+    mount -o bind $basedir/dnsmasq /usr/sbin/dnsmasq
+    killall dnsmasq >/dev/null 2>&1
+    /usr/sbin/dnsmasq 2>&1 &
+  fi
+}
+
+start_sub_daemon() {
+  killall $daemon_sub_sh >/dev/null 2>&1
+  chmod +x /tmp/$daemon_sub_sh && /tmp/$daemon_sub_sh 2>&1 &
+}
+
+start_kcptun() {
+  if [ ! -f "/opt/bin/kcptun" ]; then
+    return
+  fi
+  killall kcptun >/dev/null 2>&1
+  /opt/bin/kcptun -conn 4 -sockbuf 8388608 -l [::1]:8388 -r "[::1]:3333" -l ":8388" -key "$key" -mtu 1350 -sndwnd 192 -rcvwnd 900 -crypt xor -mode fast3 -dscp 0 -datashard 0 -parityshard 0 -autoexpire 0 -nocomp  >/dev/null 2>&1 &
+}
+
+ntp_log() {
+  LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
+  logger -t "【ntpd时间同步】" "$LOGTIME"
+}
+
+inet_check() {
+  #wget --spider --quiet https://www.google.com/favicon.ico -O - >/dev/null 2>&1
+  #echo $?
+  r=$(curl --silent --show-error --connect-timeout 5 -I https://www.google.com | grep -E "HTTP\/.+ 200 OK")
+  if [ -z "$r" ]; then
+    echo "1"
+  else
+    echo "0"
+  fi
+}
+
+total_secs=$(read_total_secs)
+if [ -z "$total_secs" ]; then
+  total_secs=0
+fi
+sleep_secs=2
+selfkill_secs=3600 # must be even number
+ntp_secs=600 # must be even number
+inet_check_interval=60
+inet_fail_count=0
+inet_fail_max=3 # this value will be increased on restart
+
+while true; do
+  if [[ ! -f "/opt/bin/kcptun" && -f "$basedir/kcptun" ]]; then
+    logger -s -t "【 本地应用守护】" "找不到/opt/bin/kcptun, 重新链接!"
+    ln -s $basedir/kcptun /opt/bin/kcptun
+  fi
+  if [[ ! -f "/opt/bin/udp2raw" && -f "$basedir/udp2raw" ]]; then  
+    logger -s -t "【 本地应用守护】" "找不到/opt/bin/udp2raw, 重新链接!"
+    ln -s $basedir/udp2raw /opt/bin/udp2raw
+  fi
+  if [ -z "$(pgrep udp2raw)" ]; then
+    logger -s -t "【 本地应用守护】" "udp2raw没有启动, 重新开始!"
+    start_sub_daemon
+  fi
+  if [ -z "$(pgrep $daemon_sub_sh)" ]; then
+    logger -s -t "【 sub-daemon 本地应用守护】" "没有启动, 重新开始!"
+    start_sub_daemon
+  fi
+  if [ -z "$(pgrep kcptun)" ]; then
+    logger -s -t "【 本地应用守护】" "kcptun没有启动, 重新开始!"
+    start_kcptun
+  fi
+
+  # crond daemon - no logging output
+  if [ $(ps | grep -E "[c]rond -l 15" | wc -l) -eq 0 ]; then
+    logger -s -t "【 本地应用守护】" "crond不输出日志!"
+    killall crond >/dev/null 2>&1
+    crond -l 15 >/dev/null 2>&1 &
+  fi
+  orig_crond_proc_id="$(ps | grep -E "[c]rond$" | awk {'print$1'})"
+  if [ ! -z "$orig_crond_proc_id" ]; then
+    kill $orig_crond_proc_id
+  fi
+
+  if [ "$(expr $total_secs \% $ntp_secs)" -eq "0" ]; then    
+    ntpd -n -q -p cn.pool.ntp.org >/dev/null 2>&1
+    LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
+    logger -t "【ntpd时间同步】" "$LOGTIME"
+  fi
+
+  if [ "$(expr $total_secs \% $inet_check_interval)" -eq "0" ]; then
+    if [ "$(inet_check)" -eq "0" ]; then
+      if [ $inet_fail_count -ge 1 ]; then
+        logger -t "【科学上网】" "恢复正常"
+      fi
+      inet_fail_count=0
+      inet_fail_max=3
+    else
+      inet_fail_count=$(expr $inet_fail_count \+ 1)
+      logger -t "【科学上网】" "连续失败$inet_fail_count次"
+      if [ $inet_fail_count -ge $inet_fail_max ]; then
+        inet_fail_count=0
+	if [ $inet_fail_max -ge 10 ]; then
+	  logger -t "【重启WAN】" "尝试连接google.com失败, 达到连续失败次数$inet_fail_max"
+	  restart_wan
+	else
+	  logger -t "【自动重启kcptun/udp2raw】" "原因：无法正常访问google.com"
+	  restart_dnsmasq
+          start_sub_daemon
+	  start_kcptun
+	fi
+	inet_fail_max=$(expr $inet_fail_max \+ 1) # max failures increased by 1
+      fi
+    fi
+  fi
+  
+  sleep $sleep_secs
+  total_secs=$(expr $total_secs \+ $sleep_secs)
+  $(write_total_secs $total_secs)
+  if [[ $(expr $total_secs \% $selfkill_secs) -eq 0 && ! -z "$(pgrep $daemon_sub_sh)" ]]; then
+    total_mins=$(expr $total_secs \/ 60)
+    logger -t "【main-daemon守护】" "已经运行$total_mins分钟, 自重启."
+    killall $daemon_sh >/dev/null 2>&1
+  fi
+
+done
+EOF
+
+# install daemons!!!
+chmod +x /tmp/$daemon_sh && /tmp/$daemon_sh 2>&1 &
+restart_dnsmasq
+
+exit 0
